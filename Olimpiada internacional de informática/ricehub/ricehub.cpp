711746f293ea96285c2095f085b82d4d4d2fc702
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2024-11-09 17:37:35 -0400

    Solución a ricehub.
    Era tan fácil y yo aquí complicándome solo con un árbol de segmentos con propagación floja.

--- Olimpiada internacional de informática/ricehub/ricehub.cpp
+++ Olimpiada internacional de informática/ricehub/ricehub.cpp
@@ -3,51 +3,34 @@
 using namespace std;
 int besthub(int R, int L, int X[], long long B){
   long long n = R;
-  if(n == 1) return 1;
-  vector<long long> a(n);
-  for(long long i = 0; i < n; i++){
-     a[i] = X[i];
+  vector<long long> a, Suma;
+  for(long long i = 0; i <= n; i++){
+    a.push_back(X[i]);
+    Suma.push_back(a.back());
+    if(i > 0) Suma.back() += Suma[i - 1];
   }
-  long long Mayor = 1;
-  vector<long long> Distancias(n, 0);
+  int Mejor = -2;
+  long long Resta = 0;
   for(long long i = 0; i < n; i++){
-    Distancias[i] = 0;
-    for(long long j = i - 1; j > -1; j--){
-      Distancias[j] = a[i] - a[j] + Distancias[j + 1];
+    long long Izquierda = i, Derecha = n - 1, Tentativo = i;
+    Resta += a[i];
+    while(1){
+      if(Izquierda >= (Derecha + 1)) break;
+      long long Promedio = (Izquierda + Derecha) / 2;
+      long long zd = (Promedio + i) / 2;
+      long long Distancia = 0;
+      Distancia += Suma[Promedio] - Suma[zd] - a[zd] * (Promedio - zd);
+      if(zd > 0) Distancia -= Suma[zd - 1];
+      if(i > 0) Distancia += Suma[i - 1];
+      Distancia += a[zd] * (zd - i);
+      //Distancia -= a[zd] * (Promedio - i + 1);
+      if(Distancia <= B){
+        Izquierda = Promedio + 1;
+        Tentativo = Promedio;
+      } else Derecha = Promedio - 1;
+      //cout<<i<<" "<<Promedio<<" "<<Distancia<<"\n";
     }
-    for(long long j = i + 1; j < n; j++){
-      Distancias[j] = a[j] - a[i] + Distancias[j - 1];
-    }
-    for(long long j = i; j > -1; j--){
-      if(Distancias[j] > B) break;
-      long long Tenemos = i - j + 1, Aun_hay = B - Distancias[j];
-      long long Izquierda = i, Derecha = n - 1, Mejor = i;
-      while(1){
-        if(Izquierda >= (Derecha + 1)) break;
-        long long Promedio = (Izquierda + Derecha) / 2;
-        if(Distancias[Promedio] <= Aun_hay){
-          Izquierda = Promedio + 1;
-          Mejor = Promedio;
-        } else Derecha = Promedio - 1;
-      }
-      Tenemos += Mejor - i;
-      Mayor = max(Mayor, Tenemos);
-    }
-    for(long long j = i + 1; j < n; j++){
-      if(Distancias[j] > B) break;
-      long long Tenemos = j - i, Aun_hay = B - Distancias[j];
-      long long Izquierda = 0, Derecha = i, Mejor = i;
-      while(1){
-        if(Izquierda >= (Derecha + 1)) break;
-        long long Promedio = (Izquierda + Derecha) / 2;
-        if(Distancias[Promedio] <= Aun_hay){
-          Derecha = Promedio - 1;
-          Mejor = Promedio;
-        } else Izquierda = Promedio + 1;
-      }
-      Tenemos += i - Mejor + 1;
-      Mayor = max(Mayor, Tenemos);
-    }
+    Mejor = max(Mejor, (int)(Tentativo - i + 1));
   }
-  return Mayor;
+  return Mejor;
 }
