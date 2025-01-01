dc3d286ebcebe1e0c7e63230fa6d323d58eb93ca
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2025-01-01 12:58:21 -0400

    Las optimizaciones no permitieron aumentar mucho la cantidad de dígitos de precisión.

--- Problemas/La tarea del Agus en progreso.cpp
+++ Problemas/La tarea del Agus en progreso.cpp
@@ -7,25 +7,21 @@ int main(){
     cin>>n;
     vector<long long> d(c, 1);
     for(long long i = 1; i < c; i++) d[i] = (d[i - 1] * 10) % n;
-    deque< vector< vector<long long> > > PD(2, vector< vector<long long> >(2, vector<long long>(n, 2222222222222222)));
-    PD[0][1][0] = 0;
+    deque< vector<long long> > PD(2, vector<long long>(n, 2222222222222222));
+    PD[0][0] = 0;
     for(long long i = c - 2; i > -1; i--){
         swap(PD[0], PD[1]);
         for(long long j = 0; j < n; j++){
-            PD[0][0][j] = 2222222222222222;
-            PD[0][1][j] = 2222222222222222;
-        }
-        PD[0][1][0] = 0;
-        for(long long j = 0; j < n; j++){
-            long long m0 = 2222222222222222, m1 = m0;
+            long long m = 2222222222222222;
+            PD[0][j] = 2222222222222222;
             for(long long k = 0; k < 10; k++){
-                m0 = min(m0, PD[1][(long long)(k > 0)][(j + d[i] * k) % n] + k);
-                m1 = min(m1, PD[1][1][(j + d[i] * k) % n] + k);
+                long long So = 0;
+                if(k == 0 and PD[1][(j + d[i] * k) % n] == 0) So = 2222222222222222;
+                m = min(m, PD[1][(j + d[i] * k) % n] + k + So);
             }
-            PD[0][0][j] = min(PD[0][0][j], m0);
-            PD[0][1][j] = min(PD[0][1][j], m1);
+            PD[0][j] = min(PD[0][j], m);
         }
     }
-    cout<<PD[0][0][0];
+    cout<<PD[0][0];
     return 0;
 }
