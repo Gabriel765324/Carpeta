c8451ad6a5431d874a59ab1cfbd247c48eb4384f
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2025-02-08 16:59:37 -0400

    Primero era hacer que vayan juntos y luego era cancelar lo que esté por demás, no al revés.

--- Problemas/Concurso del 4 de febrero al 10 de febrero/F.cpp
+++ Problemas/Concurso del 4 de febrero al 10 de febrero/F.cpp
@@ -1,4 +1,3 @@
-//Mal.
 #include "bits/stdc++.h"
 using namespace std;
 int main(){
@@ -26,137 +25,214 @@ int main(){
                 Oeste++;
             }
         }
-        long long v = min(Norte, Sur), h = min(Este, Oeste);
-        Norte -= v;
-        Sur -= v;
-        Este -= h;
-        Oeste -= h;
-        if(((Norte + Sur) % 2 == 1) or ((Este + Oeste) % 2 == 1)){
+        bool Usado = 0;
+        string r(n, '0');
+        long long Contador = 0;
+        for(long long i = 0; i < n; i++){
+            long long Resta = 0;
+            if(Norte % 2 == 1) Resta = 1;
+            if(Contador < Norte - Resta and s[i] == 'N'){
+                if(Contador % 2 == 0) r[i] = 'R';
+                else {
+                    r[i] = 'H';
+                    Usado = 1;
+                }
+                Contador++;
+            }
+        }
+        Contador = 0;
+        if(Usado){
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Sur % 2 == 1) Resta = 1;
+                if(Contador < Sur - Resta and s[i] == 'S'){
+                    if(Contador % 2 == 0) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        } else {
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Sur % 2 == 1) Resta = 1;
+                if(Contador < Sur - Resta and s[i] == 'S'){
+                    if(Contador % 2 == 1) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        }
+        Contador = 0;
+        if(Usado){
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Este % 2 == 1) Resta = 1;
+                if(Contador < Este - Resta and s[i] == 'E'){
+                    if(Contador % 2 == 0) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        } else {
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Este % 2 == 1) Resta = 1;
+                if(Contador < Este - Resta and s[i] == 'E'){
+                    if(Contador % 2 == 1) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        }
+        Contador = 0;
+        if(Usado){
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Oeste % 2 == 1) Resta = 1;
+                if(Contador < Oeste - Resta and s[i] == 'W'){
+                    if(Contador % 2 == 0) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        } else {
+            for(long long i = 0; i < n; i++){
+                long long Resta = 0;
+                if(Oeste % 2 == 1) Resta = 1;
+                if(Contador < Oeste - Resta and s[i] == 'W'){
+                    if(Contador % 2 == 1) r[i] = 'R';
+                    else {
+                        r[i] = 'H';
+                        Usado = 1;
+                    }
+                    Contador++;
+                }
+            }
+        }
+        Norte %= 2;
+        Sur %= 2;
+        Este %= 2;
+        Oeste %= 2;
+        if(Norte != Sur or Este != Oeste){
             cout<<"NO\n";
             continue;
         }
-        long long Contador = 0;
-        string r(n, '0');
-        bool Usado = 0;
-        for(long long i = 0; i < n; i++){
-            if(Contador < v and s[i] == 'N'){
-                if(Contador % 2 == 0) r[i] = 'R';
-                else {
-                    r[i] = 'H';
-                    Usado = 1;
-                }
-                Contador++;
-            }
-        }
-        Contador = 0;
-        for(long long i = 0; i < n; i++){
-            if(Contador < v and s[i] == 'S'){
-                if(Contador % 2 == 0) r[i] = 'R';
-                else {
-                    r[i] = 'H';
-                    Usado = 1;
-                }
-                Contador++;
-            }
-        }
-        Contador = 0;
         if(Usado){
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and s[i] == 'E'){
-                    if(Contador % 2 == 0) r[i] = 'R';
-                    else r[i] = 'H';
-                    Contador++;
-                }
-            }
-            Contador = 0;
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and s[i] == 'W'){
-                    if(Contador % 2 == 0) r[i] = 'R';
-                    else r[i] = 'H';
-                    Contador++;
+            for(long long i = 0; i < n and (Norte + Sur) > 0; i++){
+                if(r[i] == '0'){
+                    if(Norte > 0 and s[i] == 'N'){
+                        r[i] = 'R';
+                        Norte--;
+                    }
+                    if(Sur > 0 and s[i] == 'S'){
+                        r[i] = 'R';
+                        Sur--;
+                    }
                 }
             }
         } else {
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and s[i] == 'E'){
-                    if(Contador % 2 == 1) r[i] = 'R';
-                    else {
+            for(long long i = 0; i < n and (Norte + Sur) > 0; i++){
+                if(r[i] == '0'){
+                    if(Norte > 0 and s[i] == 'N'){
                         r[i] = 'H';
                         Usado = 1;
+                        Norte--;
                     }
-                    Contador++;
-                }
-            }
-            Contador = 0;
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and s[i] == 'W'){
-                    if(Contador % 2 == 1) r[i] = 'R';
-                    else {
+                    if(Sur > 0 and s[i] == 'S'){
                         r[i] = 'H';
                         Usado = 1;
+                        Sur--;
                     }
-                    Contador++;
                 }
             }
         }
-        Contador = 0;
-        v = (Norte + Sur) / 2;
-        h = (Este + Oeste) / 2;
         if(Usado){
-            for(long long i = 0; i < n; i++){
-                if(Contador < v and (s[i] == 'N' or s[i] == 'S') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'R';
-                } else if((s[i] == 'N' or s[i] == 'S') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'H';
-                }
-            }
-            Contador = 0;
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and (s[i] == 'E' or s[i] == 'O') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'R';
-                } else if((s[i] == 'E' or s[i] == 'O') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'H';
+            for(long long i = 0; i < n and (Este + Oeste) > 0; i++){
+                if(r[i] == '0'){
+                    if(Oeste > 0 and s[i] == 'W'){
+                        r[i] = 'R';
+                        Oeste--;
+                    }
+                    if(Este > 0 and s[i] == 'E'){
+                        r[i] = 'R';
+                        Este--;
+                    }
                 }
             }
         } else {
-            for(long long i = 0; i < n; i++){
-                if(Contador < v and (s[i] == 'N' or s[i] == 'S') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'H';
-                    Usado = 1;
-                } else if((s[i] == 'N' or s[i] == 'S') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'R';
-                }
-            }
-            Contador = 0;
-            for(long long i = 0; i < n; i++){
-                if(Contador < h and (s[i] == 'E' or s[i] == 'O') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'H';
-                    Usado = 1;
-                } else if((s[i] == 'E' or s[i] == 'O') and r[i] == '0'){
-                    Contador++;
-                    r[i] = 'R';
+            for(long long i = 0; i < n and (Este + Oeste) > 0; i++){
+                if(r[i] == '0'){
+                    if(Oeste > 0 and s[i] == 'W'){
+                        r[i] = 'H';
+                        Usado = 1;
+                        Oeste--;
+                    }
+                    if(Este > 0 and s[i] == 'E'){
+                        r[i] = 'H';
+                        Usado = 1;
+                        Este--;
+                    }
                 }
             }
         }
-        if(!Usado) cout<<"NO\n";
-        else {
-            Usado = 0;
+        if(Usado){
+            long long vr = 0, hr = 0, vh = 0, hh = 0;
+            bool R = 0, H = 0;
             for(long long i = 0; i < n; i++){
-                if(r[i] == 'R'){
-                    Usado = 1;
+                if(r[i] == '0'){
+                    vr = vh + 1;
                     break;
                 }
+                if(r[i] == 'R'){
+                    R = 1;
+                    switch(s[i]){
+                        case 'N':
+                        vr++;
+                        break;
+                        case 'S':
+                        vr--;
+                        break;
+                        case 'E':
+                        hr++;
+                        break;
+                        case 'W':
+                        hr--;
+                    }
+                } else {
+                    H = 1;
+                    switch(s[i]){
+                        case 'N':
+                        vh++;
+                        break;
+                        case 'S':
+                        vh--;
+                        break;
+                        case 'E':
+                        hh++;
+                        break;
+                        case 'W':
+                        hh--;
+                    }
+                }
             }
-            if(Usado) cout<<r<<"\n";
+            if(vr == vh and hr == hh and R and H) cout<<r<<"\n";
             else cout<<"NO\n";
-        }
+        } else cout<<"NO\n";
     }
     return 0;
 }
