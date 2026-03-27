d3d5cb2498493a7e9ad0e9f5209d30d5bc185598
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2026-03-27 13:49:21 -0400

    Superárboles.

--- IOI/2026/supertrees/supertrees.cpp
+++ IOI/2026/supertrees/supertrees.cpp
@@ -2,7 +2,11 @@
 #include "bits/stdc++.h"
 using namespace std;
 bool Final = 0;
+int n;
 vector<int> Representantes0, Representantes1;
+vector< vector<int> > Respuesta, Copia, Grafo, Separaciones, Caminos;
+vector< pair<int, int> > Activadores;
+bitset<2222> Visitados;
 int Buscar0(int a){
 	if(Representantes0[a] == a) return a;
 	return Representantes0[a] = Buscar0(Representantes0[a]);
@@ -23,10 +27,57 @@ void Unir1(int A, int B){
 	int a = Buscar1(A), b = Buscar1(B);
 	Representantes1[b] = a;
 }
+void DFS(int Nodo, bool S__vuelve, int Activador_0, int Activador_1, int Inicio){
+	Visitados[Nodo] = 1;
+	S__vuelve = S__vuelve or Nodo == Activador_0 or Nodo == Activador_1;
+	//cerr<<Nodo<<" "<<S__vuelve<<" "<<Activador_0<<" "<<Activador_1<<"\n";
+	Caminos[Inicio][Nodo] = 1 + S__vuelve;
+	for(auto E: Grafo[Nodo]){
+		if(!Visitados[E]){
+			DFS(E, S__vuelve, Activador_0, Activador_1, Inicio);
+		}
+	}
+}
+bool Verificar(){
+	Grafo.assign(n, {});
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(Respuesta[i][j] == 1){
+				//cerr<<i<<" >> "<<j<<"\n";
+				Grafo[i].push_back(j);
+			}
+		}
+	}
+	bitset<2222> Reinicio;
+	/*for(auto E: Separaciones){
+		for(auto e: E){
+			cerr<<e<<" ";
+		}
+		cerr<<"\n";
+	}*/
+	for(int i = 0; i < n; i++){
+		//cerr<<i<<"\n";
+		DFS(i, 0, Activadores[i].first, Activadores[i].second, i);
+		/*cerr<<"\n";
+		for(auto E: Caminos[i]) cerr<<E<<" ";
+		cerr<<"\n\n";*/
+		Visitados = Visitados & Reinicio;
+		if(Copia[i] != Caminos[i]){
+			//cerr<<"No válido.";
+			return 0;
+		}
+	}
+	//cerr<<"Válido.";
+	return 1;
+}
 int construct(vector< vector<int> > p){
-	int n = p.size();
+	n = p.size();
+	Caminos.assign(n, vector<int>(n, 0));
+	Separaciones.assign(n, {});
+	Copia = p;
+	Activadores.assign(n, {-2, -2});
 	for(int i = 0; i < n; i++) Representantes0.push_back(i);
-	vector< vector<int> > Respuesta(n, vector<int>(n, 0));
+	Respuesta.assign(n, vector<int>(n, 0));
 	for(int i = 0; i < n; i++){
 		for(int j = i + 1; j < n; j++){
 			if(p[i][j] == 3) return 0;
@@ -57,28 +108,26 @@ int construct(vector< vector<int> > p){
 			Unir1(Mapeo[Representantes0[i]], Mapeo[Representantes0[j]]);
 		}
 	}
-	vector< vector<int> > Separaciones(n);
 	for(int i = 0; i < m; i++){
-		Separaciones[Buscar1(i)].push_back(i);
+		Separaciones[Buscar1(i)].push_back(iMapeo[i]);
 	}
 	for(auto E: Separaciones){
 		if(E.size() <= 1) continue;
 		if(E.size() == 2) return 0;
 		for(int i = 0; i < int(E.size()); i++){
-			int Siguiente = i + 1;
+			int Siguiente = i + 1, Anterior = i - 1;
 			if(Siguiente >= int(E.size())) Siguiente = 0;
-			Respuesta[iMapeo[E[i]]][iMapeo[E[Siguiente]]] = 1;
-			Respuesta[iMapeo[E[Siguiente]]][iMapeo[E[i]]] = 1;
+			if(Anterior < 0) Anterior = int(E.size()) - 1;
+			Activadores[E[i]].first = E[Anterior];
+			Activadores[E[i]].second = E[Siguiente];
+			Respuesta[E[i]][E[Siguiente]] = 1;
+			Respuesta[E[Siguiente]][E[i]] = 1;
 		}
 	}
 	for(int i = 0; i < n; i++){
-		for(int j = i + 1; j < n; j++){
-			if(p[i][j] == 0 and (Buscar0(i) == Buscar0(j) or (Buscar1(Mapeo[i]) == Buscar1(Mapeo[j]) and Mapeo[i] != Mapeo[j]))) return 0;
-			/*if(p[i][j] == 2){
-
-			}*/
-		}
+		Activadores[i] = Activadores[Buscar0(i)];
 	}
-	build(Respuesta);
+	if(Verificar()) build(Respuesta);
+	else return 0;
 	return 1;
 }
