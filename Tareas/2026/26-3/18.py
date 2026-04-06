2ff7f5e6168f370ef27f4c80a2051b36f5acd8a2
Gabriel765324 <gabrielalbertocruznogales@gmail.com>
2026-04-06 19:34:34 -0400

    A.

--- Tareas/2026/26-3/18.py
+++ Tareas/2026/26-3/18.py
@@ -1,6 +1,6 @@
 n = int(input("Dé un número.\n"))
 Bien = n > 1
-for x in range(2, int(n ** 0.5) + 2):
+for x in range(2, int(n ** 0.5) + 1):
     Bien = Bien and n % x != 0
 if Bien:
     print("Primo.")
